452b270f96fa93d5be088a148f1fe1360f1220b8
Jeovane dos Santos Santos <jeovane.ssantos@outlook.com>
2022-08-08 16:25:23 -0300

    chore: add build

--- src/config/server.js
+++ src/config/server.js
@@ -1,4 +1,4 @@
-const port = process.env.PORT || 3003
+const port = process.env.PORT || 8080 || 4000
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
